ed0a1104a0b84c751652e34ab85b39d6a31dec27
Austin Carly <austinrcarly16@gmail.com>
2020-08-07 12:35:10 -0600

    Lunch Time

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const productsCtrl = require("./productsCtrl");
+
+const {CONNECTION_STRING, SERVER_PORT} = process.env;
+const app = express();
+
+app.use(express.json());
+
+massive({
+    connectionString:CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log(`Database is connected`)
+})
+
+app.get('/api/products', productsCtrl.getProducts)
+app.post('/api/products', productsCtrl.addProduct)
+app.get('/api/single/product/:id', productsCtrl.getOneProduct)
+app.put('/api/products/:id', productsCtrl.updateProduct)
+app.delete('/api/products/:id', productsCtrl.deleteProduct)
+
+
+app.listen(SERVER_PORT, () => console.log (`You have found the six fingered port on server ${SERVER_PORT}`))
---
+++ server/productsCtrl.js
@@ -0,0 +1,63 @@
+const { findAllByAltText } = require("@testing-library/react")
+
+module.exports = {
+    getProducts: (req, res) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.get_all_product()
+         .then(products => res.status(200).send(products))
+         .catch(err => {
+             res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
+          console.log(err)
+         });
+    },
+    addProduct: (req,res) => {
+        const dbInstance = req.app.get('db');
+        const {name, price, image_url} = req.body;
+
+        dbInstance.add_product([name, price, image_url])
+        .then(() => res.sendStatus(200))
+        .catch(err => {
+          res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
+          console.log(err)
+        });
+    },
+    getOneProduct: (req,res) => {
+        const dbInstance = req.app.get('db');
+        const { id } = req.params;
+
+        dbInstance.get_product(id)
+        .then(product => res.status(200).send(product))
+        .catch(err => {
+            res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
+            console.log(err)
+          });
+    },
+    updateProduct:(req, res) => {
+        const dbInstance = req.app.get('db');
+        const { params, query } = req;
+
+        dbInstance.update_product([params.id, query.desc])
+        .then(() => res.sendStatus(200))
+        .catch(err => {
+            res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
+            console.log(err)
+          });
+    },
+    deleteProduct:(req, res) => {
+        const dbInstance = req.app.get('db');
+        const { id } = req.params;
+
+        dbInstance.delete_product(id)
+        .then(() => res.sendStatus(200))
+        .catch(err => {
+            res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
+            console.log(err)
+          });
+    }
+    
+}
+
+
+
+
